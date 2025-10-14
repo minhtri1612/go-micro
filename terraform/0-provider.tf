ae157b67a8df7f76eccab3a56ac0cb4085b2ecc8
minhtri1612 <10422119@vgu.edu.vn>
2025-10-14 22:04:01 +0700

    update local and provider for tf

--- terraform/0-provider.tf
+++ terraform/0-provider.tf
@@ -14,9 +14,23 @@ terraform {
       source  = "hashicorp/local"
       version = "~> 2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.20.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.0"
+    }    
   }
 }
 
 provider "aws" {
   region = var.region
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
--- terraform/2-locals.tf
+++ terraform/2-locals.tf
@@ -4,4 +4,10 @@ locals {
   }
 
   aws_ecr_url = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
+  env         = "staging"
+  region      = "ap-southeast-2"
+  zone1       = "ap-southeast-2a"
+  zone2       = "ap-southeast-2b"
+  eks_name    = "demo"
+  eks_version = "1.29"
 }
